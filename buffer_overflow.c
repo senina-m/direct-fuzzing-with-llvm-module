b8822a47441af06b06463ad4a6988a35495b12ed
Mariya Senina <senina.m.m@yandex.ru>
2026-04-25 21:48:00 +0000

    functions instrumentation

--- buffer_overflow.c
+++ buffer_overflow.c
@@ -34,46 +34,54 @@ int main() {
 }
 */
 
+#include <string.h>
+
+void do_something(char* data) {
+    data[0] = 0;
+}
+
+void inner(char *data) {
+    char buf[5];
+    do_something(buf);
+    strcpy(buf, data);  // уязвимость
+}
+
+void middle(char *data) {
+    inner(data);  // вызывает уязвимую
+}
+
+void outer() {
+    middle("exploit");  // вызывает middle
+}
+
+void safe() {
+    int x = 0;
+}
+
+int main(int argc, char** argv) {
+    safe();
+    if (argv[0] == "A") safe();
+    if (argv[0] == "B") {int x = 2; return 0;}
+    if (argv[0] == 1) outer();
+    else safe();
+    return 0;
+}
+
+// #include <stdio.h>
 // #include <string.h>
 
-// void inner(char *data) {
-//     char buf[5];
-//     strcpy(buf, data);  // уязвимость
-// }
-
-// void middle(char *data) {
-//     inner(data);  // вызывает уязвимую
-// }
-
-// void outer() {
-//     middle("exploit");  // вызывает middle
-// }
-
-// void safe() {
-//     int x = 0;
+// void trigger_vulnerability(char *input) {
+//     char buf[8];
+//     strcpy(buf, input);
 // }
 
 // int main() {
-//     outer();
-//     safe();
+//     char input[128] = {0};
+//     if (fgets(input, sizeof(input), stdin) == NULL) return 0;  // ← этот блок
+
+//     if (input[0] != 'A') return 0;  // ← этот блок
+//     if (input[1] != 'B') input[1] = 'C';
+
+//     trigger_vulnerability(input);
 //     return 0;
 // }
-
-#include <stdio.h>
-#include <string.h>
-
-void trigger_vulnerability(char *input) {
-    char buf[8];
-    strcpy(buf, input);  // <-- уязвимость (buffer overflow)
-}
-
-int main() {
-    char input[128] = {0};
-    if (fgets(input, sizeof(input), stdin) == NULL) return 0;
-
-    if (input[0] != 'A') return 0;
-    if (input[1] != 'B') input[1] = 'C'; // ← этот блок
-
-    trigger_vulnerability(input);
-    return 0;
-}
